a850a0bcb5bfc9dd907d71fe8d96c1c6b7617a2d
Charlinson <charlinson2013@gmail.com>
2025-02-16 17:15:51 -0500

    test: añade prueba para verificar el inicio correcto de la aplicación

---
+++ src/test/java/com/banco/gateway/ApiGatewayApplicationTest.java
@@ -0,0 +1,14 @@
+package com.banco.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class ApiGatewayApplicationTest {
+
+    @Test
+    void contextLoads() {
+        // Test para verificar que la aplicación se inicia correctamente
+    }
+}
